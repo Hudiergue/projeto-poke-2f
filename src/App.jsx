202c9bc5fd49788e1fddbce3dbae6ae1aaf2fd01
Hudiergue <rm95395@estudante.fieb.edu.br>
2025-05-16 12:59:31 +0000

    flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { Routes, Route  } from "react-router-dom";
-import Home from "./home";
+import { Routes, Route  } from 'react-router-dom';
+import Home from './Home'
+
 const App = () => {
+
   return (
     <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home/>} />
     </Routes>
+
   )
 }
- export default App
+ 
+export default App
--- src/home.jsx
+++
@@ -1,8 +0,0 @@
-const Home = () => {
-    return (
-        <div>
-            <p>Routes blod Routes </p>
-        </div>
-    )
-}
-export default Home
